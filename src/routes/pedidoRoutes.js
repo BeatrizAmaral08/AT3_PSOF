8aea537832e3d317879a629114525c77984fe836
GABRIEL ANEQUINI VIANA <gabriel.viana24@senaisp.edu.br>
2026-05-22 16:08:17 -0300

    desenvolvimento do Routes

---
+++ src/routes/pedidoRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import pedidoController from "../controllers/pedidoController.js";
+const pedidoRoutes = Router();
+
+pedidoRoutes.post('/', pedidoController.criar);
+pedidoRoutes.get('/', pedidoController.selecionar);
+pedidoRoutes.post("/:pedidoId/itens", pedidoController.adicionarItem);
+pedidoRoutes.put("/:itemId", pedidoController.editarItem);
+pedidoRoutes.delete("/:Id", pedidoController.deletar);
+pedidoRoutes.put("/:pedidoId/status", pedidoController.atualizarStatus);
+
+export default pedidoRoutes;
